edb5decbdbe91ea3545a410f40b666f9d8de82c3
zetao.zhang <zetao.zhang@sophgo.com>
2025-12-24 10:02:28 +0800

    Fix bug: The uncertain output of bm_version leads to incorrect CHIP_STATUS information.

--- source/pbmsec/deb/opt/sophon/bmsec/commands/getBasicInf.sh
+++ source/pbmsec/deb/opt/sophon/bmsec/commands/getBasicInf.sh
@@ -7,16 +7,16 @@
 unset seNCtrl_SUB_INFOS
 declare -a seNCtrl_SUB_INFOS
 
-. ${seNCtrl_PWD}/commands/runCmd.sh all "if [ -e /sys/class/bm-tpu/bm-tpu0/device/npu_usage ]; then cat /sys/class/bm-tpu/bm-tpu0/device/npu_usage | grep -oE '[0-9]+' | head -1; else echo "NAN"; fi;\
-mpstat -P ALL 1 1 | awk '/Average/ && /all/ {print 100 - \$12}';\
-free -m | awk '/Mem/ {printf \"%.2f\", (1 - (\$7/\$2)) * 100}';echo "";\
-result=\$(sudo cat /sys/kernel/debug/ion/bm_npu_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
-result=\$(sudo cat /sys/kernel/debug/ion/bm_vpu_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
-result=\$(sudo cat /sys/kernel/debug/ion/bm_vpp_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
-echo \$((\$(cat /sys/class/thermal/thermal_zone0/temp) / 1000))/\$((\$(cat /sys/class/thermal/thermal_zone1/temp) / 1000));\
-if [ -e /proc/device-tree/tsdma* ]; then echo "BM1684X"; else echo "BM1684"; fi;\
-if [ -e /sbin/bm_version ]; then head -n 3 /sbin/bm_version; else head -n 3 /bm_bin/bm_version; fi | bash;\
-result=\$(bm-smi --noloop --file /dev/shm/chip_status.info |grep Fault |wc -l); rm -rf /dev/shm/chip_status.info; if [ \$result -eq 1 ]; then echo "FAULT"; else echo "ACTIVE"; fi;\
+. ${seNCtrl_PWD}/commands/runCmd.sh all "echo -n getbiNO0:;if [ -e /sys/class/bm-tpu/bm-tpu0/device/npu_usage ]; then cat /sys/class/bm-tpu/bm-tpu0/device/npu_usage | grep -oE '[0-9]+' | head -1; else echo "NAN"; fi;\
+echo -n getbiNO1:;mpstat -P ALL 1 1 | awk '/Average/ && /all/ {print 100 - \$12}';\
+echo -n getbiNO2:;free -m | awk '/Mem/ {printf \"%.2f\", (1 - (\$7/\$2)) * 100}';echo "";\
+echo -n getbiNO3:;result=\$(sudo cat /sys/kernel/debug/ion/bm_npu_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
+echo -n getbiNO4:;result=\$(sudo cat /sys/kernel/debug/ion/bm_vpu_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
+echo -n getbiNO5:;result=\$(sudo cat /sys/kernel/debug/ion/bm_vpp_heap_dump/summary 2>/dev/null | sed -n 's/.*rate:\([0-9]*\)%.*/\1/p');if [ -z \"\$result\" ]; then echo NAN; else echo \$result; fi;\
+echo -n getbiNO6:;echo \$((\$(cat /sys/class/thermal/thermal_zone0/temp) / 1000))/\$((\$(cat /sys/class/thermal/thermal_zone1/temp) / 1000));\
+echo -n getbiNO7:;if [ -e /proc/device-tree/tsdma* ]; then echo "BM1684X"; else echo "BM1684"; fi;\
+echo -n getbiNO8:;if [ -e /sbin/bm_version ]; then head -n 3 /sbin/bm_version; else head -n 3 /bm_bin/bm_version; fi | bash;\
+echo -n getbiNO9:;result=\$(bm-smi --noloop --file /dev/shm/chip_status.info |grep Fault |wc -l); rm -rf /dev/shm/chip_status.info; if [ \$result -eq 1 ]; then echo "FAULT"; else echo "ACTIVE"; fi;\
 " &> /dev/null
 
 printf "%-3s%-8s%-15s%-7s%-7s%-7s%-7s%-7s%-7s%-12s%-12s\n" \
@@ -35,7 +35,16 @@ for ((i = 0; i < $seNCtrl_ALL_SUB_NUM; i++)); do
             version_info="$version_info_s"
         fi
     fi
-    mapfile -t seNCtrl_SUB_INFOS < <(echo "${seNCtrl_RUN_LOG[$i]}")
+    seNCtrl_SUB_INFOS[0]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO0:' | sed 's/getbiNO0://')
+    seNCtrl_SUB_INFOS[1]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO1:' | sed 's/getbiNO1://')
+    seNCtrl_SUB_INFOS[2]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO2:' | sed 's/getbiNO2://')
+    seNCtrl_SUB_INFOS[3]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO3:' | sed 's/getbiNO3://')
+    seNCtrl_SUB_INFOS[4]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO4:' | sed 's/getbiNO4://')
+    seNCtrl_SUB_INFOS[5]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO5:' | sed 's/getbiNO5://')
+    seNCtrl_SUB_INFOS[6]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO6:' | sed 's/getbiNO6://')
+    seNCtrl_SUB_INFOS[7]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO7:' | sed 's/getbiNO7://')
+    seNCtrl_SUB_INFOS[8]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO8:' | sed 's/getbiNO8://')
+    seNCtrl_SUB_INFOS[9]=$(echo "${seNCtrl_RUN_LOG[$i]}" | grep 'getbiNO9:' | sed 's/getbiNO9://')
     printf "%-3s%-8s%-15s%-7s%-7s%-7s%-7s%-7s%-7s%-12s%-10s\n" \
            "$(($i + 1))" "${seNCtrl_SUB_INFOS[7]}" "${version_info}" "${seNCtrl_SUB_INFOS[1]}%" "${seNCtrl_SUB_INFOS[0]}%" "${seNCtrl_SUB_INFOS[2]}%" "${seNCtrl_SUB_INFOS[3]}%" "${seNCtrl_SUB_INFOS[4]}%" "${seNCtrl_SUB_INFOS[5]}%" "${seNCtrl_SUB_INFOS[6]}" "${seNCtrl_SUB_INFOS[9]}"
 
